08a37fd9c3545b7930a2f3b89933b871f78ab704
Madhavb <maddy4chat@gmail.com>
2013-06-05 14:27:17 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dcaa7487c752612eb9c4f2c54c66fa46bbd52d89c6c8c5c0ba16281552110240dda6f7cd3ef5191b02ce98baa8dc4d4499cfb36d4647d70f7826562c86262e87'
---
+++ vendor/bundle/gems/json-1.8.0/ext/json/ext/parser/siteconf20130604-94845-1a2i01g.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130604-94845-t6mpss"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
